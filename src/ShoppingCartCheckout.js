3de4ae7304f19cbe70fd8fc76a11df62b8374e57
Chris <caston88@gmail.com>
2023-06-02 12:39:27 +0100

    ShoppingCartCheckout: Passing test for 3x Strawberries discounted to £4.50 each

--- src/ShoppingCartCheckout.js
+++ src/ShoppingCartCheckout.js
@@ -25,14 +25,18 @@ class ShoppingCartCheckout {
 
   calculateTotalPrice() {
     let total = 0;
-    
+
     this.shoppingCart.forEach((item) => {
       if (item.product.code === 'FR1') {
         const buyOneGetOneFreePrice =
           Math.ceil(item.quantity / 2) * item.product.price;
 
         total += buyOneGetOneFreePrice;
+      } else if (item.product.code === 'SR1' && item.quantity >= 3) {
+        
+        total += 4.5 * item.quantity;
       } else {
+        
         total += item.product.price * item.quantity;
       }
     });
